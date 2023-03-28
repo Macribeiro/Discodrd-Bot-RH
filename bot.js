7dc3d3bd653ef7df80a158f13030f4cf0a78aa2b
Macribeiro <matheusclaudinoribeiro@gmail.com>
2023-03-28 13:34:33 -0300

    mais alterações minimalistas pq eu tenho toc

--- bot.js
+++ bot.js
@@ -27,8 +27,6 @@ client.on("threadCreator", async function (message) {
             reason: `${message.content}`,
         });
 
-        const threadId = thread.id
-
         message.thread.join(threadId)
         message.thread.send({ content: `*Data de Abertura: ${new Date().toISOString().split('T')[0]}*\nOlá, ${message.author.username}! \nRecebemos a sua reclamação e estamos com um time para analisá-la.\n:thumbsup_tone1: :thumbsup_tone1: :thumbsup_tone1: :thumbsup_tone1:\n\nMotivo do contato: ${message.content}`, allowedMentions: { repliedUser: true } })
         console.log(`Created thread: ${thread.name} | ${thread.id}`);
